b2a61ebb7733c06d754bffaec8c02e2cbba9c5e4
IT21167614 <92454064+Nimsaramahagedara@users.noreply.github.com>
2022-09-20 17:00:13 +0530

    improvements in navbar

--- js/script.js
+++ js/script.js
@@ -8,9 +8,24 @@ function menu(){
          icon.setAttribute("class","fa-solid fa-x");
          menu[0].style.visibility = "visible";
          menu[1].style.visibility = "visible";
+         scrollFunction();
     }else{
          icon.setAttribute("class","fa-solid fa-bars");
+         scrollFunction();
          menu[0].style.visibility = "hidden";
          menu[1].style.visibility = "hidden";
     }
  }
+
+
+function scrollFunction() {
+     const m = document.getElementsByClassName("box1")[0];
+     const m2 = document.getElementsByClassName("box2")[0];
+  if (icon.className == "fa-solid fa-x") {
+    m.style.top = "170px";
+    m2.style.top = "290px";
+  } else {
+    /*m.style.top = "150px";*/
+    m2.style.top = "170px";
+  }
+}
